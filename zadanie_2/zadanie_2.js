ab8e19fdd0a652b7457fd9bc4906b02a21265007
Michał Stasiński <mstasinski@gwo.pl>
2019-08-09 11:42:19 +0200

    add new exercise

---
+++ zadanie_2/zadanie_2.js
@@ -0,0 +1,10 @@
+/* Polecenie: 
+    Zadanie podobne do zadania  ale teraz 
+
+     Napisz funkcję która w parametrze bedzie przyjmowała zmieną typu referencyjnego array(tablica)
+     wywołaj funkcję dwa razy wypisując w konsoli wszystkie elementy tablicy numbers1 i numbers2
+     const numbers = [2, 5, 7, 10, 34, 16, 879, 1].
+     const numbers1 = [1,2,3].
+     
+
+ */
---
+++ zadanie_3/zadanie_3.js
@@ -0,0 +1,8 @@
+/* Polecenie: 
+     Napisz funkcję która w parametrze bedzie przyjmowała zmieną typu prostego number i  typu referencyjnego array(tablica)
+     Funkcja ma zwracać nową tablicę  utworzoną na postawie array z elementami które są wieksze od num
+
+     Wskazówka: Co to są High Order Function?
+     Poznaj je i wykorzystaj jedną z nich.
+ */
+
